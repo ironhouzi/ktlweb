e3322a8450b4c5dd4ae3938a76b210c8a9206dae
Robin <robinds@student.matnat.uio.no>
2016-02-03 09:10:38 +0100

    Add simple search templates

--- home/views.py
+++ home/views.py
@@ -34,7 +34,7 @@ def search(request):
     except EmptyPage:
         search_results = paginator.page(paginator.num_pages)
 
-    return render(request, 'demo/search_results.html', {
+    return render(request, 'search.html', {
         'search_query': search_query,
         'search_results': search_results,
         'search_picks': search_picks,
