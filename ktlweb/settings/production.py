94d8d691788fb794944aab55eb61d676f94c9e3f
Robin <robinds@student.matnat.uio.no>
2016-02-23 09:13:55 +0100

    Correct email settings for sendgrid

--- ktlweb/settings/production.py
+++ ktlweb/settings/production.py
@@ -15,8 +15,14 @@ DEFAULT_FILE_STORAGE = 'ktlweb.settings.s3storage.AWSMedia'
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 
 EMAIL_HOST = os.getenv('EMAIL_HOST', '')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_USER = os.getenv(
+    'SENDGRID_USERNAME',
+    os.getenv('EMAIL_HOST_USER', '')
+)
+EMAIL_HOST_PASSWORD = os.getenv(
+    'SENDGRID_PASSWORD',
+    os.getenv('EMAIL_HOST_PASSWORD', '')
+)
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'true').lower() in ('true', '1',)
 
