cd72c5795fa0e337ddc987249f0a50e87f8bce0e
Robin Skahjem-Eriksen <robin@skahjem-eriksen.no>
2019-12-15 13:33:49 +0100

    Revert "Support WAGTAILADMIN_STATIC_FILE_VERSION_STRINGS"
    This reverts commit 9ecd69562808153170d4d3de9f77b8245cf00838.

--- ktlweb/settings/s3storage.py
+++ ktlweb/settings/s3storage.py
@@ -1,12 +1,10 @@
+from storages.backends.s3boto import S3BotoStorage
 import os
 
-from storages.backends.s3boto import S3BotoStorage
-from django.contrib.staticfiles.storage import ManifestFilesMixin
 
-
-class AWSMedia(ManifestFilesMixin, S3BotoStorage):
+class AWSMedia(S3BotoStorage):
     bucket_name = os.environ['AWS_MEDIA']
 
 
-class AWSStatic(ManifestFilesMixin, S3BotoStorage):
+class AWSStatic(S3BotoStorage):
     bucket_name = os.environ['AWS_STATIC']
