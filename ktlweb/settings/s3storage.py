e85b45dc4ae97cf2b37d5839aed3e0b0d8342b62
Robin Skahjem-Eriksen <robin@skahjem-eriksen.no>
2020-03-11 20:22:47 +0100

    Upgrade to boto3

--- ktlweb/settings/s3storage.py
+++ ktlweb/settings/s3storage.py
@@ -1,10 +1,10 @@
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 import os
 
 
-class AWSMedia(S3BotoStorage):
+class AWSMedia(S3Boto3Storage):
     bucket_name = os.environ['AWS_MEDIA']
 
 
-class AWSStatic(S3BotoStorage):
+class AWSStatic(S3Boto3Storage):
     bucket_name = os.environ['AWS_STATIC']
