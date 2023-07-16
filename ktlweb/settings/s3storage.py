3d4be08d6e7c05d27935b6ffee14b5f28c6a43b6
Robin Skahjem-Eriksen <robin@skahjem-eriksen.no>
2023-07-16 21:15:55 +0200

    change: S3Boto3Storage.querystring_auth

--- ktlweb/settings/s3storage.py
+++ ktlweb/settings/s3storage.py
@@ -4,8 +4,10 @@ import os
 
 class AWSMedia(S3Boto3Storage):
     bucket_name = os.environ['AWS_MEDIA']
+    querystring_auth = True
 
 
 class AWSStatic(S3Boto3Storage):
     bucket_name = os.environ['AWS_STATIC']
     default_acl = 'public-read'
+    querystring_auth = False
